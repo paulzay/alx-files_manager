187ef185779be8f1813d07f1a6944ba2845a06dc
Paul Ogolla <paulotieno2@gmail.com>
2024-10-15 18:48:02 +0300

    update redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,23 +1,27 @@
-import { createClient } from 'redis';
+const { createClient } = require('redis');
+const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
+    this.connect = true;
     this.client = createClient({ url: 'redis://localhost:6379' });
     this.client.on('error', (error) => {
       console.log(error);
+      this.client = false;
     });
-    this.client.connect();
+    this.client.on('connect', () => {
+      this.connect = true;
+    });
   }
 
   isAlive() {
-    if (this.client.connect) {
-      return true;
-    }
+    if (this.connect) return true;
     return false;
   }
 
   async get(key) {
-    const value = await this.client.get(key);
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
     return value;
   }
 
