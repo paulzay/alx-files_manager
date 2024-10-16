d04d37b3a3bca1b541839379ab6d2cddc3fdbf90
Paul Ogolla <paulotieno2@gmail.com>
2024-10-16 23:13:34 +0300

    create users route for post

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const UserController = require('../controllers/UsersController');
 
 const router = express.Router();
 
 router.route('/status').get(AppController.getStatus);
 
 router.route('/stats').get(AppController.getStats);
+
+router.route('/users').post(UserController.postNew);
 
 export default router;
