800bfe6d1e3264d716fcb9c0bd77ee89e979fd47
Paul Ogolla <paulotieno2@gmail.com>
2024-10-16 23:33:24 +0300

    port change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import router from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port  ${port}`);
