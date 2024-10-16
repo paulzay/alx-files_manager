3c15e5b55f885ef38b25520690d1df1860c68240
Paul Ogolla <paulotieno2@gmail.com>
2024-10-16 23:13:18 +0300

    handle json and urlencoded

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ import express from 'express';
 import router from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port  ${port}`);
 });
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
 
 export default app;
